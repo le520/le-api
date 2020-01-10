00226248de69ed994a3693afdf9839cf2218f21f
le520 <863485501@qq.com>
2020-01-10 21:48:38 +0800

    Add VimTips api.

---
+++ app/controller/VimTips.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+
+class VimTips extends BaseController
+{
+    public function index()
+    {
+        $file_name = "static/data/data.txt";
+        $fp = fopen($file_name, 'r');
+        $num = 0;
+        $data = array();
+        while (!feof($fp)) {
+            $data[$num] = trim(fgets($fp));
+            $num++;
+        }
+        fclose($fp);
+        $num = mt_rand(0,$num-2);
+        $reData['status']="success";
+        $reData['data']=$data[$num];
+        return json_encode($reData,JSON_UNESCAPED_UNICODE);
+    }
+}
